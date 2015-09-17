4ea84c4be4476e0aea213612021a1c280b1c1d1c
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2015-09-16 20:51:11 -0500

    h-es

--- assets/js/hache-es.js
+++ assets/js/hache-es.js
@@ -22,7 +22,7 @@ $(function(){
     }
     // make conversation
     $('#story').typed({
-        strings: ["Las instrucciones deben ser vistas a solas.^2000 \nSi alguien está contigo,^300 por favor cierra \nel portal^1000 y vuelve en otro momento.^5000 \n\nSi aún estás leyendo, supondremos \nque nadie te acompaña.^2000 \nEl secreto es:^1000 \n\nRay no existe, son los papás.^3000 \nEn la petición, el usuario escribe \nen secreto la respuesta.^200 \nSi comienzas la petición con un punto '.', \ntu respuesta se escribirá en secreto\n disfrazada por el texto '\nRay, por favor responde...'^500 \nEntonces Ray no adivina, sólo es tu cómplice \n para sorprender y asustar a tus amigos. \n\nPrueba ingresando la pregunta \n'¿Dónde vivimos?'^1000 \ny escribe \n'.Planeta Tierra.' en la petición.^1000 \nNotarás que la petición se disfraza... ^2000 \n\nDiviértete."],
+        strings: ["Las instrucciones deben ser vistas a solas.^2000 \nSi alguien está contigo,^300 por favor cierra \nel portal^1000 y vuelve en otro momento.^5000 \n\nSi aún estás leyendo, supondremos \nque nadie te acompaña.^2000 \nEl secreto es:^1000 \n\nRay no existe, son los papás.^3000 \nEn la petición, el usuario escribe la \nrespuesta oculta.^200 \nSi comienzas la petición con un punto '.', \ntu respuesta se escribirá en secreto\n disfrazada por el texto '\nRay, por favor responde...'^5000 \n\nRay no adivina, sólo es tu cómplice \npara sorprender y asustar a tus amigos. \n\nPrueba ingresando la pregunta \n'¿Dónde vivimos?'^1000 \ny escribe \n'.Planeta Tierra.' en la petición.^1000 \nNotarás que la petición se disfraza... ^2000 \n\nDiviértete."],
         typeSpeed: 15,
         backDelay: 500,
         loop: false,
